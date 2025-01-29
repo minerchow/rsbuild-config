8920b7b85f4a94137859bd00a5af2c6dcb44b71e
zhouming <minerchow@126.com>
2025-01-29 22:33:29 +0800

    first

---
+++ rsbuild.config.js
@@ -0,0 +1,85 @@
+import { defineConfig } from '@rsbuild/core';
+import { pluginReact } from '@rsbuild/plugin-react';
+import { rspack } from '@rspack/core';
+import { pluginSass } from '@rsbuild/plugin-sass';
+console.log(process.env.CROSS_ENV)
+export default defineConfig({
+  plugins: [pluginReact(), pluginSass()],
+  entry: 'index.js',
+  output: {
+    // externals: {
+    //   'react-dom/client': 'ReactDOM',
+    //   'react': 'React'
+    // },
+    polyfill: 'usage'
+  },
+  tools: {
+    rspack: {
+      plugins: [new rspack.HtmlRspackPlugin({
+        title: "My HTML Template",
+        template: 'public/index.html',
+        filename: 'index.html'
+      })]
+
+    },
+
+  },
+  html: false,
+  resolve: {
+    alias: {
+      '@': './src',
+    },
+  },
+  performance: {
+    removeConsole: process.env.CROSS_ENV === 'production' ? true : false,
+    chunkSplit: {
+      strategy: 'custom',
+      splitChunks: {
+        cacheGroups: {
+          react: {
+            test: /node_modules[\\/](react|react-dom|react-router-dom)[\\/]/,
+            name: 'react',
+            minChunks: 1,
+            priority: -2,
+            chunks: 'all',
+            enforce: true
+          },
+          antd: {
+            test: /node_modules[\\/](antd)[\\/]/,
+            name: 'antd',
+            minChunks: 1,
+            priority: -9,
+            chunks: 'all',
+            enforce: true
+          },
+
+          vendor: {
+            name: 'vendor',
+            chunks: 'all',
+            minChunks: 3,
+            priority: -10,
+            test: /[\\/]node_modules[\\/]/,
+            reuseExistingChunk: true,
+          },
+          default: {
+            chunks: 'all',
+            test: /[\\/]src[\\/]/,
+            minChunks: 3,
+            priority: -20,
+            reuseExistingChunk: true,
+            name: 'common'
+          }
+        },
+      },
+    },
+    bundleAnalyze:{
+      analyzerMode: 'server',
+      openAnalyzer: false,
+    }
+  },
+  source: {
+    define: {
+      CROSS_ENV: JSON.stringify(process.env.CROSS_ENV)
+    }
+  }
+});
---
+++ src/App.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { Button } from 'antd';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+// 使用 React.lazy 进行懒加载
+const Home = React.lazy(() => import('@/pages/home'));
+const About = React.lazy(() => import('@/pages/about'));
+const Contact = React.lazy(() => import('@/pages/contract'));
+import './App.scss';
+
+const App = () => {
+  useEffect(() => {
+    //@ts-ignore
+    console.log('App mounted',CROSS_ENV);
+    return () => {
+      console.log('App unmounted');
+    };
+  }, []);
+
+  return (
+    <div className="content">
+      <h1>Rsbuild with React12</h1>
+      <p>Start building amazing things with Rsbuild.</p>
+      <Button>12</Button>
+      <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      {/* 使用 React.Suspense 包裹 Routes，设置加载时的提示信息 */}
+      <React.Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </React.Suspense>
+    </Router>
+    </div>
+  );
+};
+
+export default App;
